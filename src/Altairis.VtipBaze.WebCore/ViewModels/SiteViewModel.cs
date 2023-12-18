b401a8b048eca6ed42bbb598a317626300c02ba3
Tomáš Herceg <tomas.herceg@riganti.cz>
2023-12-18 15:28:15 +0100

    Migrated the master page

---
+++ src/Altairis.VtipBaze.WebCore/ViewModels/SiteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DotVVM.Framework.ViewModel;
+using System.Web.Security;
+
+namespace Altairis.VtipBaze.WebCore.ViewModels
+{
+    public abstract class SiteViewModel : DotvvmViewModelBase
+    {
+        public abstract string PageTitle { get; }
+
+        public void SignOut()
+        {
+            FormsAuthentication.SignOut();
+            Context.RedirectToLocalUrl("/");
+        }
+    }
+}
+
