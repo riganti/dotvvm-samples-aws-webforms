e0142e6ea6e6ed9afa927f1db1ac9cd9cc73bab9
Tomáš Herceg <tomas.herceg@riganti.cz>
2023-12-18 15:28:15 +0100

    Migrated the master page

--- src/Altairis.VtipBaze.WebCore/DotvvmStartup.cs
+++ src/Altairis.VtipBaze.WebCore/DotvvmStartup.cs
@@ -1,4 +1,5 @@
 using DotVVM.Framework.Configuration;
+using DotVVM.Framework.ResourceManagement;
 using DotVVM.Framework.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,9 +31,15 @@ namespace Altairis.VtipBaze.WebCore
         private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
         {
             // register custom resources and adjust paths to the built-in resources
+            config.Resources.RegisterStylesheetUrl("jquery-ui-css", "https://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/themes/south-street/jquery-ui.css", null);
+            config.Resources.RegisterStylesheetFile("site-css", "Styles/main.min.css", dependencies: new[] { "jquery-ui-css" });
+
+            config.Resources.RegisterScriptFile("site", "Scripts/Site/ui.js", dependencies: new[] { "jquery-ui" });
+            config.Resources.RegisterScriptFile("jquery-ui", "Scripts/jquery-ui-1.10.3.min.js", dependencies: new[] { "jquery" });
+            config.Resources.RegisterScriptFile("jquery", "Scripts/jquery-2.0.3.min.js");
         }
-		
-		public void ConfigureServices(IDotvvmServiceCollection options)
+
+        public void ConfigureServices(IDotvvmServiceCollection options)
         {
             options.AddDefaultTempStorages("temp");
 		}
